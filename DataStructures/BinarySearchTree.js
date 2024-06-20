8e2b6ff5307b5d0ed5a24056c87fe1d43f6b0e67
mussab_tafal <mussab_tafal@hotmail.com>
2024-06-20 15:00:59 +0400

    Understanding Tree Traversals

--- DataStructures/BinarySearchTree.js
+++ DataStructures/BinarySearchTree.js
@@ -59,15 +59,88 @@ class BinarySearchTree {
       }
     }
   }
+
+  bfsTraverse() {
+    if (!this.root) return null;
+    var node = this.root,
+      queue = [],
+      data = [];
+    queue.push(node);
+
+    while (queue.length) {
+      node = queue.shift();
+      data.push(node.value);
+
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+    return data;
+  }
+
+  dfsTraversepreOrder() {
+    let values = [];
+    let current = this.root;
+
+    function traverse(node) {
+      values.push(node.value);
+      if (node.left) {
+        traverse(node.left);
+      }
+      if (node.right) {
+        traverse(node.right);
+      }
+    }
+
+    traverse(current);
+    return values;
+  }
+
+  dfsTraversePostOrder() {
+    let values = [];
+    let current = this.root;
+
+    function traverse(node) {
+      if (node.left) {
+        traverse(node.left);
+      }
+      if (node.right) {
+        traverse(node.right);
+      }
+      values.push(node.value);
+    }
+
+    traverse(current);
+    return values;
+  }
+
+  dfsTraverseInOrder() {
+    let values = [];
+    let current = this.root;
+
+    function traverse(node) {
+      if (node.left) {
+        traverse(node.left);
+      }
+      values.push(node.value);
+      if (node.right) {
+        traverse(node.right);
+      }
+    }
+
+    traverse(current);
+    return values;
+  }
 }
 
 var tree = new BinarySearchTree();
 tree.insert(10);
-tree.insert(5);
-tree.insert(13);
-tree.insert(11);
-tree.insert(2);
-tree.insert(16);
-tree.insert(7);
-console.log(tree.find(10));
+tree.insert(6);
+tree.insert(15);
+tree.insert(3);
+tree.insert(8);
+tree.insert(20);
+// console.log(tree.find(10));
 console.log(tree);
+console.log(tree.bfsTraverse());
+console.log(tree.dfsTraversePostOrder());
+console.log(tree.dfsTraverseInOrder());
